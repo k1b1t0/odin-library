02d9922a78ce1671d0d122cbb334bfca6de78279
k1b1t0 <153260776+k1b1t0@users.noreply.github.com>
2026-02-07 10:31:09 +0700

    Add remove button

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+const shelf = document.querySelector('.shelf')
 const myLibrary = [] 
 
 function Book(title, author, pages, read) {
@@ -14,13 +15,6 @@ Book.prototype.setId = function() {
     this.id = crypto.randomUUID()
 }
 
-Book.prototype.info = function() {
-    if (this.read) {
-        return `${this.id}: ${this.title} by ${this.author}, ${this.pages} pages, read`
-    }
-    return `${this.id}: ${this.title} by ${this.author}, ${this.pages} pages, not read yet`
-}
-
 function addBookToLibrary(title, author, pages, read) {
     const book = new Book(title, author, pages, read)
     book.setId()
@@ -28,13 +22,57 @@ function addBookToLibrary(title, author, pages, read) {
 }
 
 function displayAllBooks() {
-    for (book of myLibrary) {
-        console.log(book.info())
-    }
+    myLibrary.forEach(function(current, index, array) {
+        const card = document.createElement('div')
+        card.classList.add('card')
+
+        const id = document.createElement('p')
+        id.textContent = current.id
+        id.style.fontSize = 'small'
+        card.appendChild(id)
+
+        const title = document.createElement('h1')
+        title.textContent = current.title
+        card.appendChild(title)
+
+        const author = document.createElement('p')
+        author.textContent = current.author
+        card.appendChild(author)
+
+        const pages = document.createElement('p')
+        pages.textContent = current.pages + ' page(s)'
+        card.appendChild(pages)
+
+        const read = document.createElement('p')
+        if (current.read) {
+            read.textContent = "read"
+            read.style.color = 'green'
+        } else {
+            read.textContent = "not read yet"
+            read.style.color = 'red'
+        }
+        card.appendChild(read)
+
+        const delBtn = document.createElement('button')
+        delBtn.textContent = 'delete'
+        card.appendChild(delBtn)
+
+        card.dataset.index = current.id
+        shelf.appendChild(card)
+    })
 }
 
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, false)
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, false)
+shelf.addEventListener('click', (e) => {
+    if (e.target.matches('button')) {
+        const card = e.target.closest(`.card`)
+        const id = card.dataset.id
+        shelf.removeChild(card)
+        myLibrary.splice(id, 1)
+    }
+})
+
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, true)
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, true)
 addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, false)
 addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, false)
 addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, false)
